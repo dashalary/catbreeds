be1e265d98364b4d3af94d766fb40773040c2bfd
dashalary <dk2800@nyu.edu>
2020-09-28 11:23:19 -0400

    will add environment.rb

--- lib/catbreeds/api.rb
+++ lib/catbreeds/api.rb
@@ -1,10 +1,15 @@
 class CatBreeds::API 
+require 'pry'
 
 
-
-
-
-
+    def self.get_breeds
+    resp = HTTParty.get("https://api.thecatapi.com/v1/breeds", {
+        headers: {"Authorization" => "#{ENV['API_KEY']}"},
+    })
+    binding.pry
+    breeds = resp["breeds"]
+    CatBreeds::Breed.new_from_collection(breeds)
+    end
 
 
 
