6d306bce4c0a86e01e2e9bfb868c0e10d521ec3b
dashalary <dk2800@nyu.edu>
2020-09-28 10:14:18 -0400

    commit3

--- lib/catbreeds/cli.rb
+++ lib/catbreeds/cli.rb
@@ -1,6 +1,11 @@
 class CLI 
 
-
+def menu 
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Welcome to CatBreeds!"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Please enter "breeds" to see a list of all breeds of domestic cats or "exit" to exit."
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 
 
